e754478d892e49fa8c77f647a7d2017c17b6dda8
marycamara <marycamara101@gmail.com>
2024-05-01 21:29:02 +0100

    title

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-
 function NavBar() {
   return (
-     <div class="topnav">
-            <div class="topnav-right">
-                <a class="active" href="index.html">Home</a>
-                <a href="#about-me">About me</a>
-                <a href="#projects">Projects</a>
-                <a href="contact.html">Contact</a>
-            </div>
+    <div>
+      <div className="topnav">
+        <div className="topnav-right">
+          <a className="active" href="index.html">Home</a>
+          <a href="#about-me">About Me</a>
+          <a href="#projects">Projects</a>
+          <a href="contact.html">Contact</a>
         </div>
-          
-  
+      </div>
+      <header>Mary Camara</header>
+    </div>
   );
 }
 
