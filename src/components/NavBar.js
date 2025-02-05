44c29daf34d0ae4cd447184e28a32edab312ab0a
marycamara <marycamara101@gmail.com>
2025-02-05 22:05:46 +0000

    Fixed the header title and it's now on the same line as the navigation links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import "../styles/navbar.css";
+import Contacts from "./Contacts";  // Make sure the path is correct
+import HeaderTitle from './HeaderTitle';
 
 function NavBar() {
   return (
-    <div>
-      <div className="topnav">
-        <div className="topnav-right">
-          <a className="active" href="index.html">Home</a>
-          <a href="#about-me">About Me</a>
-          <a href="#projects">Projects</a>
-          <a href="contact.html">Contact</a>
-        </div>
+  <> 
+      {/* Header Title Component */}
+     <div className="topnav">
+    <div className='HeaderTitle'>
+      <HeaderTitle />
       </div>
-      <header>Mary Camara</header>
-    </div>
+      
+
+    
+    
+      {/* Navigation Links */}
+        <div className="nav-links">
+        <a className="active" href="index.html">Home</a>
+        <a href="#about-me">About Me</a>
+        <a href="#projects">Projects</a>
+        <a href="contact.html">Contact</a>
+      </div>
+
+      {/* Add the Contacts component */}
+      <div className="contact-links">
+        <Contacts />
+      </div>
+      </div>
+      </>
   );
 }
 
